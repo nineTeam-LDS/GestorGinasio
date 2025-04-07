576b747800172494c064bfc74430b7535cab7d6d
Pedro Cruz <pseruz@gmail.com>
2025-04-08 00:40:01 +0100

    Create MenuPrincipal.cs

---
+++ View/MenuPrincipal.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class MenuPrincipal
+{
+    public void ExibirMenu()
+    {
+        Console.WriteLine("1. Gerir Sócios");
+        Console.WriteLine("2. Gerar Relatórios");
+        Console.WriteLine("3. Sair");
+    }
+}
