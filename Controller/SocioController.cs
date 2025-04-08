cdb9ef799a94a4f0b307555d73763211b121be69
Pedro Cruz <pseruz@gmail.com>
2025-04-08 02:50:17 +0100

    Create SocioController.cs

---
+++ Controller/SocioController.cs
@@ -0,0 +1,22 @@
+public class SocioController
+{
+    private readonly Socio socio;
+    private readonly SocioView view;
+
+    public SocioController(Socio socio, SocioView view)
+    {
+        this.socio = socio;
+        this.view = view;
+        socio.DadosAtualizados += OnDadosAtualizados;
+    }
+
+    public void AtualizarSocio(string nome, string email)
+    {
+        socio.AtualizarDados(nome, email);
+    }
+
+    private void OnDadosAtualizados(object sender, EventArgs e)
+    {
+        view.MostrarDadosSocio(socio);
+    }
+}
