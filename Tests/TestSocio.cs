99536cb8f94f4965aac528e47a2f38984c3b7124
Pedro Cruz <pseruz@gmail.com>
2025-04-08 02:48:38 +0100

    Create TestSocio.cs

---
+++ Tests/TestSocio.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class TestSocio
+{
+    [Test]
+    public void AtualizarDados_DeveLancarEvento()
+    {
+        var socio = new Socio();
+        bool eventoDisparado = false;
+
+        socio.DadosAtualizados += (sender, e) => eventoDisparado = true;
+
+        socio.AtualizarDados("Pedro", "pedro@exemplo.com");
+
+        Assert.IsTrue(eventoDisparado);
+    }
+}
